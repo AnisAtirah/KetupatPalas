6692172779d4406bb2947c4cb1f1b90d559aa829
Liyana-S2 <23002170@siswa.um.edu.my>
2026-04-25 00:46:57 +0800

    edit kt last part tu utk vercel

--- KetupatPalas/app.py
+++ KetupatPalas/app.py
@@ -351,5 +351,5 @@ def api_recommendation():
 
 # ─── Entry point ──────────────────────────────────────────────────────────────
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=False)
